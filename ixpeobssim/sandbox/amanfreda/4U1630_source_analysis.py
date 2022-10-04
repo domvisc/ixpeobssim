0d026bd9b398f40458d587c1eb8d110eac0fc4e1
alberto_manfreda <albertom@pi.infn.it>
2022-10-04 09:13:26 +0200

    Sandbox

--- ixpeobssim/sandbox/amanfreda/4U1630_source_analysis.py
+++ ixpeobssim/sandbox/amanfreda/4U1630_source_analysis.py
@@ -3,14 +3,18 @@ from tkinter import N
 
 import numpy
 import os
-from functools import reduce
 
 from ixpeobssim.binning.misc import xBinnedLightCurve
+from ixpeobssim.binning.polarization import xBinnedCountSpectrum
 from ixpeobssim.utils.matplotlib_ import plt, color_wheel_ar, color_wheel_mpr
 from ixpeobssim.utils.time_ import met_to_mjd
+from ixpeobssim.utils.logging_ import logger
 import ixpeobssim.core.pipeline as pipeline
 import ixpeobssim.evt.xspec_ as xspec_
 from ixpeobssim.core.hist import xScatterPlot
+from ixpeobssim import IXPEOBSSIM_CONFIG_ASCII
+from ixpeobssim.srcmodel.spectrum import xXspecModel
+
 
 
 DATA_FOLDER = '/home/alberto/xpe/xpedata/4U1630-472/01250401/event_l2'
@@ -18,13 +22,8 @@ FILE_NAMES = ['ixpe01250401_det1_evt2_v03.fits',
               'ixpe01250401_det2_evt2_v03.fits',
               'ixpe01250401_det3_evt2_v03.fits']
 FILE_LIST = [os.path.join(DATA_FOLDER, file_name) for file_name in FILE_NAMES]
-
-
-def combine_light_curves(lc1, lc2):
-    """
-    """
-    lc1 += lc2
-    return lc1
+TSTART = 178067582
+TSTOP = 178915982
 
 def apply_exposure_mask(lc, min_exposure=60):
     """
@@ -75,6 +74,10 @@ def draw_time_boundaries(boundaries, mjd=True):
     for i, (t0, t1) in enumerate(zip(boundaries[:-1], boundaries[1:])):
         plt.gca().axvspan(t0, t1, alpha=0.2, color=color_wheel_mpr(i))
 
+def space_select(*file_list, innerrad=0., rad=20., suffix='spsel'):
+    return pipeline.xpselect(*file_list, suffix=suffix, innerrad=innerrad,
+                             rad=rad)
+
 def energy_select(*file_list, emin=2., emax=8., suffix=None):
     return pipeline.xpselect(*file_list, suffix=suffix, emin=emin, emax=emax)
 
@@ -88,8 +91,7 @@ def time_select(*file_list, tmin=None, tmax=None, suffix=None):
 def build_light_curve(*file_list, tbins=230, suffix=None, label=None):
     lc_files = pipeline.xpbin(*file_list, algorithm='LC', tbins=tbins,
                               irfname='v11:ixpe:obssim')
-    light_curves = [xBinnedLightCurve(lc_file) for lc_file in lc_files]
-    light_curve = reduce(combine_light_curves, light_curves)
+    light_curve = xBinnedLightCurve.from_file_list(lc_files)
     apply_exposure_mask(light_curve)
     draw_light_curve(light_curve, suffix=suffix, label=label)
     return light_curve
@@ -104,7 +106,29 @@ def select_time_regions(*file_list, boundaries=None):
     if boundaries is None:
         return
     for i, (t0, t1) in enumerate(zip(boundaries[:-1], boundaries[1:])):
-        pipeline.xpselect(*file_list, tmin=t0, tmax=t1, suffix=time_suffix(i))
+        pipeline.xpselect(*file_list, ltimeupdate=True, ltimealg='LTSUM',
+                          tmin=t0, tmax=t1, suffix=time_suffix(i))
+
+def pha_bin(*file_list):
+    output_files = []
+    output_files.append(pipeline.xpbin(*file_list, algorithm='PHA1',
+                                       irfname='ixpe:obssim:v11', ebins=20))
+    output_files.append(pipeline.xpbin(*file_list, algorithm='PHA1Q',
+                                       irfname='ixpe:obssim:v11', ebins=20))
+    output_files.append(pipeline.xpbin(*file_list, algorithm='PHA1U',
+                                       irfname='ixpe:obssim:v11', ebins=20))
+    return output_files
+
+def spectral_fit(model, *pha_files):
+    pipeline.xpxspec(*pha_files, model=model.expression,
+                     params=model.parameters, error=False)
+
+def spectro_polarimetric_fit(expression, params, *pha_files):
+    """
+    """
+    pipeline.xpxspec(*pha_files, model=expression,
+                     params=params, error=False)
+
 
 def run():
     """
@@ -123,14 +147,25 @@ def run():
                                       label=elabel(5., 8.),tbins=tbins)
     draw_hardness_ratio(low_energy_lc, high_energy_lc,
                         label='[5-8] keV / [2-3] keV', mjd=True)
+    # Divide the dataset in four time regions, based on the hardnessa ratio:
     t = all_energy_lc.TIME
-    boundaries = [min(t), 178288000., 178347500., 178742060., max(t)]
+    boundaries = [TSTART, 178288000., 178347500., 178742060., TSTOP]
     draw_time_boundaries(boundaries, mjd=True)
-    print('Selecting time zones...')
-    print(all_energy_files)
-    input()
     select_time_regions(*all_energy_files, boundaries=boundaries)
-
+    spectral_model = xXspecModel.from_file(os.path.join(
+                         IXPEOBSSIM_CONFIG_ASCII, 'tbabs_diskbb_parfile.par'))
+    for i in range(len(boundaries) -1):
+        logger.info(f'Fitting time bin {i}')
+        file_list = [file_path.replace('.fits', f'_{time_suffix(i)}.fits') \
+                     for file_path in all_energy_files]
+        pha_binned_files = pha_bin(*file_list)
+        count_spectrum = xBinnedCountSpectrum.from_file_list(pha_binned_files[0])
+        count_spectrum.plot(label=f'time bin {i}')
+        #spectral_fit(*pha_binned_files, model=spectral_model)
+        #expression = "tbabs*diskbb*pollin"
+        #params = [7.57, 1.45, 3.87, 0.02, 0.02]
+        #spectro_polarimetric_fit(expression, params, *pha_binned_files)
+    plt.legend()
 
 
 if __name__ == '__main__':
